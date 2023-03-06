e7c2ee192ef749fb8ae5476f3428dc0254d2240a
NET_NguyenDucHanh <RKDEV20221107@toanleh.onmicrosoft.com>
2023-03-06 22:48:28 +0700

    abc

---
+++ GameVui_DoanHinhGiongNhau/GameVui_DoanHinhGiongNhau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameVui_DoanHinhGiongNhau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
